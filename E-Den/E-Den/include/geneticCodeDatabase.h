4de93a1da79556feeb87f5d7531062b75b741e9b
Twiad <twiad@elane2k.com>
2010-01-18 19:33:29 +0100

    added geneticDatabase files

---
+++ E-Den/E-Den/include/geneticCodeDatabase.h
@@ -0,0 +1,18 @@
+// geneticCodeDatabase.h
+// by Franz Koehler 2010
+
+#ifndef _E_DEN_GENETIC_CODE_DATABASE_HEADER_
+#define _E_DEN_GENETIC_CODE_DATABASE_HEADER_
+
+#include "geneticCode.h"
+#include "tinyxml.h"
+
+namespace EDen {
+  
+  class GeneticCodeDatabase {
+
+  };
+
+} // namespace
+
+#endif
---
+++ E-Den/E-Den/src/geneticCodeDatabase.cpp
@@ -0,0 +1,8 @@
+// geneticCodeDatabase.cpp 
+// by Franz Koehler 2010
+
+#include "geneticCodeDatabase.h"
+
+namespace EDen {
+
+}; // namespace

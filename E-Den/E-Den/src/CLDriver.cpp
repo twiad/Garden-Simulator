bd524cf172cd16dac149d9f483512d556a93b695
Twiad <twiad@elane2k.com>
2010-12-13 03:17:55 +0100

    testbench-nel now does some nonsense opencl calculations in the background now

--- E-Den/E-Den/src/CLDriver.cpp
+++ E-Den/E-Den/src/CLDriver.cpp
@@ -15,6 +15,55 @@ namespace EDen {
     }
   };
 
+  char* oclLoadProgSource(const char* cFilename, const char* cPreamble, size_t* szFinalLength)
+  {
+      // locals 
+      FILE* pFileStream = NULL;
+      size_t szSourceLength;
+
+      // open the OpenCL source code file
+      #ifdef _WIN32   // Windows version
+          if(fopen_s(&pFileStream, cFilename, "rb") != 0) 
+          {       
+              return NULL;
+          }
+      #else           // Linux version
+          pFileStream = fopen(cFilename, "rb");
+          if(pFileStream == 0) 
+          {       
+              return NULL;
+          }
+      #endif
+
+      size_t szPreambleLength = strlen(cPreamble);
+
+      // get the length of the source code
+      fseek(pFileStream, 0, SEEK_END); 
+      szSourceLength = ftell(pFileStream);
+      fseek(pFileStream, 0, SEEK_SET); 
+
+      // allocate a buffer for the source code string and read it in
+      char* cSourceString = (char *)malloc(szSourceLength + szPreambleLength + 1); 
+      memcpy(cSourceString, cPreamble, szPreambleLength);
+      if (fread((cSourceString) + szPreambleLength, szSourceLength, 1, pFileStream) != 1)
+      {
+          fclose(pFileStream);
+          free(cSourceString);
+          return 0;
+      }
+
+      // close the file and return the total length of the combined (preamble + source) string
+      fclose(pFileStream);
+      if(szFinalLength != 0)
+      {
+          *szFinalLength = szSourceLength + szPreambleLength;
+      }
+      cSourceString[szSourceLength + szPreambleLength] = '\0';
+
+      return cSourceString;
+  }
+
+
   CLDriver::CLDriver() {
     // constant initialization values;
     cSourceFile = "VectorAdd.cl";
@@ -75,28 +124,10 @@ namespace EDen {
     }
     
     // Read the OpenCL kernel in from source file
-    FILE* fp = fopen("VectorAdd.cl", "r");
-    fseek (fp , 0 , SEEK_END);
-    const size_t lSize = ftell(fp);
-    rewind(fp);
-    unsigned char* buffer;
-    buffer = (unsigned char*) malloc (lSize);
-    fread(buffer, 1, lSize, fp);
-    fclose(fp);
- 
-    cl_int status;
-    cpProgram = clCreateProgramWithBinary(cxGPUContext, 
-                1, &cdDevice, 
-                &lSize, (const unsigned char**)&buffer, 
-                &ciErr1, &ciErr2);
-    ciErr2 = clBuildProgram(cpProgram, 0, NULL, NULL, 
-                NULL, NULL);
-    
-    checkCLErr(ciErr1,"LoadFile");
-    checkCLErr(ciErr2,"LoadFile");
-    
+    cSourceCL = oclLoadProgSource("VectorAdd.cl", "", &szKernelLength);
+
     // Create the program
-    cpProgram = clCreateProgramWithSource(cxGPUContext, 1, (const char **)&cSourceCL, &szKernelLength, &ciErr1);
+    cpProgram = clCreateProgramWithSource(cxGPUContext, 1, (const char **)&cSourceCL, &szKernelLength,&ciErr1);
     if (ciErr1 != CL_SUCCESS)
     {
         Cleanup(EXIT_FAILURE);
@@ -111,7 +142,7 @@ namespace EDen {
     ciErr1 = clBuildProgram(cpProgram, 0, NULL, NULL, NULL, NULL);
     if (ciErr1 != CL_SUCCESS)
     {
-        Cleanup(EXIT_FAILURE);
+      Cleanup(EXIT_FAILURE);
     }
 
     // Create the kernel
